4b6ad794c6382c1c4710bae0488da578462e1186
Soham334 <soham.ks@somaiya.edu>
2025-04-17 15:54:45 +0530

    Add files via upload

---
+++ admin/donate.php
@@ -0,0 +1,213 @@
+
+<?php
+// $connection = mysqli_connect("localhost:3307", "root", "");
+// $db = mysqli_select_db($connection, 'demo');
+include "../connection.php";
+include("connect.php"); 
+if($_SESSION['name']==''){
+	header("location:signin.php");
+}
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="admin.css">
+     
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>Admin Dashboard Panel</title> 
+    
+<?php
+ $connection=mysqli_connect("localhost","root","");
+ $db=mysqli_select_db($connection,'demo');
+ 
+
+
+?>
+</head>
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <!--<img src="images/logo.png" alt="">-->
+            </div>
+
+            <span class="logo_name">ADMIN</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="admin.php">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dahsboard</span>
+                </a></li>
+                <!-- <li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Content</span>
+                </a></li> -->
+                <li><a href="analytics.php">
+                    <i class="uil uil-chart"></i>
+                    <span class="link-name">Analytics</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-heart"></i>
+                    <span class="link-name">Donates</span>
+                </a></li>
+                <li><a href="feedback.php">
+                    <i class="uil uil-comments"></i>
+                    <span class="link-name">Feedbacks</span>
+                </a></li>
+                <li></li>
+                <!-- <li><a href="#">
+                    <i class="uil uil-share"></i>
+                    <span class="link-name">Share</span>
+                </a></li> -->
+            </ul>
+            
+            <ul class="logout-mode">
+                <li><a href="../logout.php">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                    <span class="link-name">Dark Mode</span>
+                </a>
+
+                <div class="mode-toggle">
+                  <span class="switch"></span>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+            <!-- <p>Food Donate</p> -->
+            <p  class ="logo" >Food <b style="color: #06C167; ">Donate</b></p>
+             <p class="user"></p>
+            <!-- <div class="search-box">
+                <i class="uil uil-search"></i>
+                <input type="text" placeholder="Search here...">
+            </div> -->
+            
+            <!--<img src="images/profile.jpg" alt="">-->
+        </div>
+        <br>
+        <br>
+        <br>
+    
+  
+
+            <div class="activity">
+               
+            <div class="location">
+                <!-- <p class="logo">Filter by Location</p> -->
+          <form method="post">
+             <label for="location" class="logo">Select Location:</label>
+             <!-- <br> -->
+            <select id="location" name="location">
+            <option value="mumbai">Mumbai</option>
+<option value="thane">Thane</option>
+<option value="palghar">Palghar</option>
+<option value="raigad">Raigad</option>
+<option value="navi_mumbai">Navi Mumbai</option>
+<option value="pune" selected>Pune</option>
+<option value="nagpur">Nagpur</option>
+<option value="nashik">Nashik</option>
+<option value="aurangabad">Aurangabad</option>
+<option value="solapur">Solapur</option>
+<option value="kolhapur">Kolhapur</option>
+<option value="sangli">Sangli</option>
+<option value="satara">Satara</option>
+<option value="amravati">Amravati</option>
+<option value="akola">Akola</option>
+<option value="jalgaon">Jalgaon</option>
+<option value="nanded">Nanded</option>
+<option value="parbhani">Parbhani</option>
+<option value="latur">Latur</option>
+<option value="beed">Beed</option>
+<option value="osmanabad">Osmanabad</option>
+<option value="wardha">Wardha</option>
+<option value="buldhana">Buldhana</option>
+<option value="dhule">Dhule</option>
+<option value="ratnagiri">Ratnagiri</option>
+<option value="sindhudurg">Sindhudurg</option>
+<option value="gondia">Gondia</option>
+<option value="chandrapur">Chandrapur</option>
+<option value="gadchiroli">Gadchiroli</option>
+<option value="yavatmal">Yavatmal</option>
+<option value="hingoli">Hingoli</option>
+<option value="washim">Washim</option>
+            </select>
+                <input type="submit" value="Get Details">
+         </form>
+         <br>
+
+         <?php
+    // Get the selected location from the form
+    if(isset($_POST['location'])) {
+      $location = $_POST['location'];
+      
+      // Query the database for people in the selected location
+      $sql = "SELECT * FROM food_donations WHERE location='$location'";
+      $result=mysqli_query($connection, $sql);
+    //   $result = $conn->query($sql);
+      
+      // If there are results, display them in a table
+      if ($result->num_rows > 0) {
+        // echo "<h2>Food Donate in $location:</h2>";
+        
+        echo" <div class=\"table-container\">";
+        echo "    <div class=\"table-wrapper\">";
+        echo "  <table class=\"table\">";
+        echo "<table><thead>";
+        echo" <tr>
+        <th >Name</th>
+        <th>food</th>
+        <th>Category</th>
+        <th>phoneno</th>
+        <th>date/time</th>
+        <th>address</th>
+        <th>Quantity</th>
+        
+    </tr>
+    </thead><tbody>";
+
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td data-label=\"name\">".$row['name']."</td><td data-label=\"food\">".$row['food']."</td><td data-label=\"category\">".$row['category']."</td><td data-label=\"phoneno\">".$row['phoneno']."</td><td data-label=\"date\">".$row['date']."</td><td data-label=\"Address\">".$row['address']."</td><td data-label=\"quantity\">".$row['quantity']."</td></tr>";
+
+        //   echo "<tr><td>" . $row["name"] . "</td><td>" . $row["phoneno"] . "</td><td>" . $row["location"] . "</td></tr>";
+        }
+        echo "</tbody></table></div>";
+      } else {
+        echo "<p>No results found.</p>";
+      }
+      
+   
+    }
+  ?>
+ </div>
+
+ 
+
+            
+            </div>
+    </section>
+
+    <script src="admin.js"></script>
+</body>
+</html>
